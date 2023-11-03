e5e7b372c99cb47364b61f193e02eda81fcc9136
eric-ud <udzhuhu.eric@gmail.com>
2023-11-03 08:19:16 +0300

    rudimentary logging

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from typing import Union
 
-from fastapi import FastAPI, Request, File, UploadFile, HTTPException
+from fastapi import FastAPI, Request, File, UploadFile, HTTPException, Header
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse, RedirectResponse, StreamingResponse
 from fastapi.templating import Jinja2Templates
@@ -14,6 +14,28 @@ import io
 from functools import reduce
 from datetime import datetime
 import zipfile
+
+import logging
+import pathlib
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+
+ch = logging.StreamHandler()
+ch.setLevel(logging.INFO)
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+
+try:
+    fh = logging.FileHandler("logs/log.txt")
+except FileNotFoundError:
+    pathlib.Path("logs").mkdir(exist_ok=True)
+    fh = logging.FileHandler("logs/log.txt")
+
+ch.setLevel(logging.INFO)
+fh.setFormatter(formatter)
+logger.addHandler(fh)
 
 app = FastAPI()
 
@@ -31,17 +53,23 @@ def create_upload_file(
 ) -> StreamingResponse:
     client_host = request.client.host
 
+    logger.info(f"Файл {excel.filename} загружен из {client_host}")
+
     wb = io.BytesIO(excel.file.read())
 
     try:
         openpyxl_reader = openpyxl.load_workbook(wb)
     except zipfile.BadZipFile:
+        logger.error("Загружен неизвестный тип файла или битый файл.")
         raise HTTPException(
             status_code=422,
             detail="Неизвестный тип файла или битый файл.",
         )
 
     if np.prod([ws.max_row for ws in openpyxl_reader.worksheets]) > 3_000_000:
+        logger.error(
+            "Загружено слишком много строк. Максимум 3 миллиона после перемножения."
+        )
         raise HTTPException(
             status_code=422,
             detail="Слишком много строк. Максимум 3 миллиона после перемножения.",
@@ -49,6 +77,7 @@ def create_upload_file(
 
     with pd.ExcelFile(wb, engine="openpyxl") as reader:
         if len(reader.sheet_names) < 2:
+            logger.error("Загружено слишком мало листов. Требуется минимум 2 листа.")
             raise HTTPException(
                 status_code=422,
                 detail="Слишком мало листов. Требуется минимум 2 листа.",
@@ -56,6 +85,7 @@ def create_upload_file(
         sheets = [pd.read_excel(reader, n, decimal=",") for n in reader.sheet_names]
 
     result_df = reduce(lambda x, y: x.merge(y, how="cross"), sheets)
+    logger.info(f"Файл {excel.filename} перемножен")
 
     def chunker(seq: pd.DataFrame, size: int) -> pd.DataFrame:
         for pos in range(0, len(seq), size):
@@ -70,6 +100,7 @@ def create_upload_file(
         for index, chunk in enumerate(chunker(result_df, 1_000_000)):
             chunk.to_excel(writer, sheet_name=str(index).zfill(2))
     output_xlsx.seek(0)
+    logger.info(f"Файл {excel.filename} обработан и клиент {client_host} получил ответ")
     return StreamingResponse(output_xlsx, headers=headers)
 
 
